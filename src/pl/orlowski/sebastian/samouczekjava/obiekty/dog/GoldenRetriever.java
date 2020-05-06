90fd93767f88acbc3e6a164154fdcbd049f03538
mydarkside <sebekovsky@gmail.com>
2020-05-06 09:50:51 +0200

    Gettery i Settery

---
+++ src/pl/orlowski/sebastian/samouczekjava/obiekty/dog/GoldenRetriever.java
@@ -0,0 +1,48 @@
+package pl.orlowski.sebastian.samouczekjava.obiekty.dog;
+
+public class GoldenRetriever extends Bulldog {
+    String name;
+    boolean isBig;
+    int lenghtOfLife;
+
+    public GoldenRetriever(String name, boolean isBig, int lengthOfLife) {
+        super(name, isBig, lengthOfLife);
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean isBig() {
+        return isBig;
+    }
+
+    @Override
+    public void setBig(boolean big) {
+        isBig = big;
+    }
+
+    public int getLenghtOfLife() {
+        return lenghtOfLife;
+    }
+
+    public void setLenghtOfLife(int lenghtOfLife) {
+        this.lenghtOfLife = lenghtOfLife;
+    }
+
+    @Override
+    public String toString() {
+        return "GoldenRetriever{" +
+                "name='" + name + '\'' +
+                ", isBig=" + isBig +
+                ", lenghtOfLife=" + lenghtOfLife +
+                '}';
+    }
+}
---
+++ src/pl/orlowski/sebastian/samouczekjava/obiekty/fish/Dolphin.java
@@ -0,0 +1,14 @@
+package pl.orlowski.sebastian.samouczekjava.obiekty.fish;
+
+public class Dolphin {
+    int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+}
