b2f57e2f110459440d4df74eeba259dde542a494
mydarkside <sebekovsky@gmail.com>
2020-05-07 16:30:43 +0200

    Interfejsy

---
+++ src/pl/orlowski/sebastian/samouczekjava/a06Interfejsy/Exercise/Main.java
@@ -0,0 +1,37 @@
+package pl.orlowski.sebastian.samouczekjava.a06Interfejsy.Exercise;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Main main = new Main();
+        Computation computation;
+
+        if (main.shouldMultiply()) {
+            computation = new ComputationMultiply(); // zaimplementuj brakującą klasę
+        }
+        else {
+            computation = new ComputationAdd(); // zaimplementuj brakującą klasę
+        }
+
+        double argument1 = main.getArgument();
+        double argument2 = main.getArgument();
+
+        double result = computation.compute(argument1, argument2);
+        System.out.println("Wynik: " + result);
+    }
+
+    private boolean shouldMultiply() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Czy chcesz mnożyć czy dodawać? TAK jeśli chcesz mnożyć / NIE jeżeli dodawać");
+        String multiply = input.nextLine();
+        // tutaj zapytaj użytkownika co chce zrobić (mnożenie czy dodawanie)
+        return multiply.equalsIgnoreCase("tak");
+    }
+
+    private double getArgument() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Podaj liczbę");
+        return input.nextInt(); // tutaj pobierz liczbę od użytkownika
+    }
+}
---
+++ src/pl/orlowski/sebastian/samouczekjava/a06Interfejsy/paczka/CatInterface.java
@@ -0,0 +1,5 @@
+package pl.orlowski.sebastian.samouczekjava.a06Interfejsy.paczka;
+
+public interface CatInterface extends Cat, CatEat{
+    double getWeight();
+}
