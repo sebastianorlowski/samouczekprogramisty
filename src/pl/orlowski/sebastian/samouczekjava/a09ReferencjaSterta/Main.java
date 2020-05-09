593a03b9da799798a1e1c1292e0818ac46935360
mydarkside <sebekovsky@gmail.com>
2020-05-09 12:27:39 +0200

    Referencje

---
+++ src/pl/orlowski/sebastian/samouczekjava/a09ReferencjaSterta/Main.java
@@ -0,0 +1,16 @@
+package pl.orlowski.sebastian.samouczekjava.a09ReferencjaSterta;
+
+public class Main {
+    public static void main(String[] args) {
+        Object objectX = new Object();
+        Object anotherObjectX = objectX;
+        Object objectY = new Object();
+
+        System.out.println("Object X: " + objectX);
+        System.out.println("Object X1: " + anotherObjectX);
+        System.out.println("Object Y: " + objectY);
+
+
+    }
+
+}
---
+++ src/pl/orlowski/sebastian/samouczekjava/a09ReferencjaSterta/brudnopis/Main.java
@@ -0,0 +1,31 @@
+package pl.orlowski.sebastian.samouczekjava.a09ReferencjaSterta.brudnopis;
+
+public class Main {
+    public static void main(String[] args) {
+        ChangeAttributes attribute1 = new ChangeAttributes(123);
+        ChangeAttributes attribute2 = attribute1;
+
+        System.out.println(attribute1.getAttribute());
+        System.out.println(attribute2.getAttribute());
+
+        attribute1.setAttribute(20);
+
+        System.out.println(attribute1.getAttribute());
+        System.out.println(attribute2.getAttribute());
+
+        attribute2.setAttribute(120);
+
+        System.out.println(attribute1.getAttribute());
+        System.out.println(attribute2.getAttribute());
+
+        System.out.println(attribute1);
+        System.out.println(attribute2);
+
+        String a1 = new String("a");
+        String a2 = new String ("a");
+        String a3 = "a";
+        String a4 = "a";
+
+    }
+
+}
